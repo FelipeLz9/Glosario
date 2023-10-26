9644e67423cc834f70539ec66b5db0666b450fce
Sebastián García <smasgg@gmail.com>
2023-10-26 16:05:30 -0500

    Documentación

--- src/Modelos/LectorArchivo.java
+++ src/Modelos/LectorArchivo.java
@@ -14,13 +14,24 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /**
- *
- * @author lopez
+ * Proporciona métodos para leer información desde un archivo de texto
+ * utilizando la codificación UTF-8 y realizar operaciones relacionadas con los términos contenidos
+ * en el archivo.
+ * 
+ * @author Juan Felipe López
+ * @author Sebastián García Gil
  */
 public class LectorArchivo  {
     
     Charset charset = Charset.forName("UTF-8");
     
+    /**
+     * Lee la descripción asociada a un término específico desde el archivo.
+     * 
+     * @param palabra La palabra o término cuya descripción se desea obtener.
+     * @return La descripción asociada al término si se encuentra; de lo contrario, retorna nulo.
+     * @throws IOException Si ocurre un error durante la lectura del archivo.
+     */
     public String leer(String palabra) throws IOException{
         String[] elementos = null;
         String desc = null;
@@ -40,6 +51,12 @@ public class LectorArchivo  {
         return desc;
     }
     
+    /**
+     * Lista los términos contenidos en el archivo.
+     * 
+     * @return Un ArrayList que contiene los términos disponibles en el archivo.
+     * @throws IOException Si ocurre un error durante la lectura del archivo.
+     */
     public ArrayList listarContenidoDeArchivo() throws IOException {
         ArrayList<String> elementos = new ArrayList<>();
         Path input = Paths.get("C:\\Users\\lopez\\OneDrive\\Escritorio\\Terminos.txt");
